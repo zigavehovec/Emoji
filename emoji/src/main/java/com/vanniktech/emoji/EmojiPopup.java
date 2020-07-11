abcda1a5c0ca05d452ee25cb224dc36a8d66b193
ghshenavar <45355182+ghshenavar@users.noreply.github.com>
2020-07-11 16:22:09 +0430

    EmojiPopUp: RequestApplyInsets (#452)
    Co-authored-by: Niklas Baudy <niklas.baudy@vanniktech.de>

--- emoji/src/main/java/com/vanniktech/emoji/EmojiPopup.java
+++ emoji/src/main/java/com/vanniktech/emoji/EmojiPopup.java
@@ -34,6 +34,7 @@ import com.vanniktech.emoji.listeners.OnSoftKeyboardOpenListener;
 import static android.os.Build.VERSION.SDK_INT;
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
 import static android.os.Build.VERSION_CODES.O;
+import static androidx.core.view.ViewCompat.requestApplyInsets;
 import static com.vanniktech.emoji.Utils.backspace;
 import static com.vanniktech.emoji.Utils.checkNotNull;
 
@@ -278,6 +279,7 @@ import static com.vanniktech.emoji.Utils.checkNotNull;
     if (!popupWindow.isShowing()) {
       // this is needed because something might have cleared the insets listener
       start();
+      requestApplyInsets(context.getWindow().getDecorView());
       show();
     } else {
       dismiss();
